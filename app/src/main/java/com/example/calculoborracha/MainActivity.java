17921684d94df85ad850876f52d49ca05b101975
Rubens <rubao.ma@gmail.com>
2020-07-29 09:52:52 -0300

    adicionado transicao entre a MainActivity e PesoActivity

--- app/src/main/java/com/example/calculoborracha/MainActivity.java
+++ app/src/main/java/com/example/calculoborracha/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.calculoborracha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void chamarTelaCalculoPeso(View view){
+        Intent intent = new Intent(this, PesoActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/calculoborracha/PesoActivity.java
+++ app/src/main/java/com/example/calculoborracha/PesoActivity.java
@@ -2,13 +2,39 @@ package com.example.calculoborracha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class PesoActivity extends AppCompatActivity {
+
+    private TextInputEditText textDiametroFerro;
+    private TextInputEditText textDiametroBorracha;
+    private TextInputEditText textComprimentoBorracha;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_peso);
+
+        //Inicializar os componentes
+        textDiametroFerro = findViewById(R.id.textDiametroFerro);
+        textDiametroBorracha = findViewById(R.id.textDiametroBorracha);
+        textComprimentoBorracha = findViewById(R.id.textComprimentoBorracha);
+
+
+
+    }
+    public void chamarTelaInicial(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void calcularPesoBorracha(){
+
     }
 }
