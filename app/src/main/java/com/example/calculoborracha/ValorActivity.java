5d8a48badca4b348872c449b88835c7b691e4a77
Rubens <rubao.ma@gmail.com>
2020-08-08 09:20:21 -0300

    adicionado metodo que torna funcional o botao tela inicial na activityValor

--- app/src/main/java/com/example/calculoborracha/ValorActivity.java
+++ app/src/main/java/com/example/calculoborracha/ValorActivity.java
@@ -2,7 +2,9 @@ package com.example.calculoborracha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ValorActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ValorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_valor);
+    }
+
+    public void chamarTelaInicial(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
